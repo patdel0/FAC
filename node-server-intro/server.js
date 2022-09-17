e48dd63972bb13ced3a9fefe95d35208a8ca2936
patdel0 <zefiru@protonmail.com>
2022-09-17 20:02:07 +0100

    Add /search

--- node-server-intro/server.js
+++ node-server-intro/server.js
@@ -1,9 +1,9 @@
-const express = require('express')
+const express = require("express");
 
-const server = express()
+const server = express();
 
-server.get('/', (request, response) => {
-  const year = new Date().getFullYear()
+server.get("/", (request, response) => {
+  const year = new Date().getFullYear();
   response.send(`
     <!doctype html>
     <html>
@@ -15,11 +15,16 @@ server.get('/', (request, response) => {
         <h1>Hello, it's ${year}</h1>
       </body>
     </html>
-  `)
-})
+  `);
+});
 
-server.get('/uh-oh', (request, response) => {
-  response.status(500).send('something went wrong')
-})
+server.get("/search", (request, response) => {
+  const keyword = request.query.keyword;
+  response.send(`<p>You searched for ${keyword}</p>`);
+});
 
-module.exports = server
+server.get("/uh-oh", (request, response) => {
+  response.status(500).send("something went wrong");
+});
+
+module.exports = server;
--- node-server-intro/tests/server.tests.js
+++ node-server-intro/tests/server.tests.js
@@ -1,25 +1,36 @@
-const test = require('node:test')
-const assert = require('node:assert')
-const server = require('../server.js')
+const test = require("node:test");
+const assert = require("node:assert");
+const server = require("../server.js");
 
-test('home route returns expected page', async () => {
-  const app = server.listen(1987)
-  const response = await fetch('http://localhost:1987')
-  app.close()
+test("home route / returns expected page", async () => {
+  const app = server.listen(1987);
+  const response = await fetch("http://localhost:1987");
+  app.close();
 
-  assert.equal(response.status, 200)
+  assert.equal(response.status, 200);
 
-  const body = await response.text()
-  assert.equal(/hello/i.test(body), true)
-})
+  const body = await response.text();
+  assert.equal(/hello/i.test(body), true);
+});
 
-test('uh-oh page returns status 500', async () => {
-  const app = server.listen(1989)
-  const response = await fetch('http:localhost:1989/uh-oh')
-  app.close()
+test("/uh-oh page returns status 500", async () => {
+  const app = server.listen(1989);
+  const response = await fetch("http:localhost:1989/uh-oh");
+  app.close();
 
-  assert.equal(response.status, 500)
+  assert.equal(response.status, 500);
 
-  const body = await response.text()
-  assert.equal(body, 'something went wrong')
-})
+  const body = await response.text();
+  assert.equal(body, "something went wrong");
+});
+
+test("/search returns message including keyword", async () => {
+  const app = server.listen(1989);
+  const response = await fetch("http:localhost:1989/search?keyword=bananas");
+  app.close();
+
+  assert.equal(response.status, 200);
+
+  const body = await response.text();
+  assert.match(body, /You searched for bananas/);
+});
